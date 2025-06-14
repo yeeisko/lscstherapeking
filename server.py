1f48147c27a8a2ff8bd53667a3dd4b002f5c0cf2
wist <hapuse11@gmail.com>
2025-06-14 21:31:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
+# server.py
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return send_from_directory('static', 'index.html')
+def serve_index():
+    return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
-
+    app.run(host='0.0.0.0', port=5000)
