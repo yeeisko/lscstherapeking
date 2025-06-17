7b61549bb6c114f4c6529b89af8d12b0992baa1e
wist <hapuse11@gmail.com>
2025-06-17 16:22:21 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 # Serve index.html at the root URL
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'index.html')
 
 # Serve index2.html in the /uploads/ path
 @app.route('/uploads/index2.html')
 def serve_index2():
-    return send_from_directory('uploads', 'index2.html')
+    return send_from_directory('uploads', 'index2.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
