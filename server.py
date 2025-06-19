e1529a43d4f027707373daff8ad87628e819d89c
yeeisko <hapuse11@gmail.com>
2025-06-19 16:11:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,21 @@
-from flask import Flask, send_from_directoryMore actions
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 # Serve index.html at the root URL
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'index.html')
 
-# Serve index2.html in the /uploads/ path
 # Serve index2.html at /uploads/index2.html
 @app.route('/uploads/index2.html')
 def serve_index2():
-    return send_from_directory('uploads', 'index2.html')
+    return send_from_directory('uploads', 'index2.html')
 
 # Serve index3.html at /uploads/index3.html
 @app.route('/uploads/index3.html')
 def serve_index3():
-    return send_from_directory('uploads', 'index3.html')
+    return send_from_directory('uploads', 'index3.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
