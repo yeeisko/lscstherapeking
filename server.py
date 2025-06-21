ad602df58a5c845a74a1d4fef67a6b5657e830f5
yeeisko <hapuse11@gmail.com>
2025-06-21 16:03:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)  # corrected to __name__
+app = Flask(__name__)  # corrected to __name__
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/uploads/index2.html')
 def serve_index2():
-    return send_from_directory('uploads', 'index2.html')
+    return send_from_directory('uploads', 'index2.html')
 
-if __name__ == '__main__':  # corrected to __name__ and __main__
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':  # corrected to __name__ and __main__
+    app.run(host='0.0.0.0', port=5000)
