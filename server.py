aca5ff366576115e419da027ca9e4b7a64f5fdd6
yeeisko <hapuse11@gmail.com>
2025-06-22 20:37:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)  # corrected to __name__
+app = Flask(__name__)  # corrected to __name__
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/uploads/index1.html')
 def serve_index2():
-    return send_from_directory('uploads', 'index1.html')
+    return send_from_directory('uploads', 'index1.html')
 
-if __name__ == '__main__':  # corrected to __name__ and __main__
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/uploads/key-matcher.html')
+def serve_key_matcher():
+    return send_from_directory('uploads', 'key-matcher.html')
+
+if __name__ == '__main__':  # corrected to __name__ and __main__
+    app.run(host='0.0.0.0', port=5000)
