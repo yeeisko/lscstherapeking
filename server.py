a6bb7429ef226451c67ca9d104e7351bb950ab38
yeeisko <hapuse11@gmail.com>
2025-07-05 21:15:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,20 +4,20 @@ app = Flask(name)
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/uploads/index1.html')
 def serve_index2():
-    return send_from_directory('uploads', 'index1.html')
+    return send_from_directory('uploads', 'index1.html')
 
 @app.route('/uploads/test.html')
 def serve_key_matcher():
-    return send_from_directory('uploads', 'test.html')
+    return send_from_directory('uploads', 'test.html')
 
 # Serve app-access.html from the current directory instead of uploads
 @app.route('/app-access.html')
 def serve_app_access():
-    return send_from_directory('.', 'app-access.html')
+    return send_from_directory('.', 'app-access.html')
 
 if name == 'main':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
